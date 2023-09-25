ae935072c83b26e00a0d12c1441982cc50a9c264
Christian Heider Nielsen <mrnaah@gmail.com>
2023-09-25 08:33:53 +0200

    handle none addr

--- jord/__init__.py
+++ jord/__init__.py
@@ -14,7 +14,7 @@ from warg import package_is_editable
 
 __project__ = "Jord"
 __author__ = "Christian Heider Lindbjerg"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __doc__ = r"""
 .. module:: jord
    :platform: Unix, Windows
