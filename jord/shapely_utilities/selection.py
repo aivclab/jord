dca9fee778e89f792251272519b14342315fac1d
Christian Heider Nielsen <mrnaah@gmail.com>
2023-11-24 13:30:11 +0100

    bump

--- jord/shapely_utilities/selection.py
+++ jord/shapely_utilities/selection.py
@@ -10,7 +10,7 @@ __all__ = ["split_enveloping_geometry"]
 def split_enveloping_geometry(
     geoms: List[shapely.geometry.base.BaseGeometry],
 ) -> Optional[
-    Tuple[shapely.geometry.base.BaseGeometry], List[shapely.geometry.base.BaseGeometry]
+    Tuple[shapely.geometry.base.BaseGeometry, List[shapely.geometry.base.BaseGeometry]]
 ]:
     """
     Splits the enveloping geometry from the rest, if any, otherwise None.
