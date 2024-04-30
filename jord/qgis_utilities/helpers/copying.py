37d8448dd320bd3b59bf7cb784946ea067abe6d9
Christian Heider Nielsen <mrnaah@gmail.com>
2024-04-30 13:57:41 +0200

    bump

--- jord/qgis_utilities/helpers/copying.py
+++ jord/qgis_utilities/helpers/copying.py
@@ -4,15 +4,15 @@ __all__ = ["deepcopy_layer"]
 
 
 def deepcopy_layer(layer) -> Any:
-  # noinspection PyUnresolvedReferences
-  from qgis.core import QgsFeatureRequest
+    # noinspection PyUnresolvedReferences
+    from qgis.core import QgsFeatureRequest
 
-  new_layer = layer.materialize(
-    QgsFeatureRequest().setFilterFids(layer.allFeatureIds())
-  )
+    new_layer = layer.materialize(
+        QgsFeatureRequest().setFilterFids(layer.allFeatureIds())
+    )
 
-  if False:
-    for r in layer.featureRendererGenerators():
-      new_layer.addFeatureRendererGenerator(r)
+    if False:
+        for r in layer.featureRendererGenerators():
+            new_layer.addFeatureRendererGenerator(r)
 
-  return new_layer
+    return new_layer
